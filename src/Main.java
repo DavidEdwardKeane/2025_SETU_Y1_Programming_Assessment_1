2686f2427d10f1a4257df5b67e943377f1807758
David Keane <davidedwardkeane@gmail.com>
2025-03-22 15:26:02 +0000

    Adds project skeleton code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class StudentInfo {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StudentInfo myStudentObject = new StudentInfo();
+        System.out.println(myStudentObject.printStars(10) + " Student Information " + myStudentObject.printStars(10));
+        System.out.println("You will be prompted to enter some details about a student.");
+
+        System.out.print("Enter Student Name: ");
+        String studentName = input.nextLine();
+        //TODO Enter code to ask user for student details as per example above and store responses
+
+        System.out.println(myStudentObject.printStars(40));
+
+        //TODO print out the student details
+
+        System.out.println(myStudentObject.printStars(40));
+        System.out.println("Vowel Count in Name: " + myStudentObject.numVowels(studentName));
+        //TODO Call appropriate methods and print information as per the example above
     }
+
+    private int numVowels(String str) {
+        /*
+        Description: This method takes a string as input and returns the count of vowels
+                       (a, e, i, o, u) in the string.
+        Algorithm: Iterate through the characters of the input string and
+                   increment a counter for each vowel encountered.
+         */
+        return -1; //you will return correct value -1 used so that code compiles
+    }
+
+    private int numWords(String str) {
+        /*
+        Description: This method takes a string as input and returns the count of words in the string.
+        Algorithm: Split the input string into words using whitespace as delimiter and
+                    return the count of resulting substrings.
+         */
+        return -1; //you will return correct value -1 used so that code compiles
+    }
+
+    private String reverseString(String str) {
+      /*
+    Description: This method takes a string as input and returns a new string with the
+                characters in reverse order.
+    Algorithm: Iterate through the characters of the input string from end to start
+                and append them to a new string.
+
+     */
+
+        return ""; //you will return correct value
+    }
+
+    public String printStars(int num) {
+        String theStars = "";
+        for(int i = 0; i<num;i++)
+            theStars += "*";
+        return theStars;
+    }
+
+    public String isAboveThreshold(double value, double threshold) {
+
+         /*
+    Description: This method takes 2 doubles as input and returns "Yes" or "No".
+    Algorithm: Checks if the value entered is above the threshold value, returns Yes if it is
+    or No if it isn't
+
+     */
+
+        return ""; //you will return correct value
+    }
+
+    //TODO You may add in any methods that you wish
 }
